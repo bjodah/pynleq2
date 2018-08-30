280db6858873bb26246c9be6a23ef67fb33b47c4
Björn Dahlgren <bjodah@gmail.com>
2018-08-30 16:06:54 +0200

    Fix prepare deply

--- scripts/prepare_deploy.sh
+++ scripts/prepare_deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 touch doc/_build/html/.nojekyll
-p LICENSE doc/_build/html/.nojekyll
+cp LICENSE doc/_build/html/.nojekyll
 git config --global user.name "drone"
 git config --global user.email "drone@nohost.com"
 mkdir -p deploy/public_html/branches/"${CI_BRANCH}" deploy/script_queue
